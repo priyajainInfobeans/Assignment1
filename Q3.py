8d78b91ba10ee14b010769d19374f5ce52fb63c9
priyajainInfobeans <40419522+priyajainInfobeans@users.noreply.github.com>
2018-06-25 11:52:55 +0530

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,42 +1,42 @@
 print "*****TELEPHONE DIRECTORY***"
-Contact1=[]
-Contact2=[]
-dict1={}
-temp=100
-n=input("Enter the number of contacts : ")
-for i in range(0,n):
-    name1=raw_input("Enter your name: ")
-    num=input("Enter your phone number: ")
+list1 = []
+list2 = []
+dict1 = {}
+temp = 100
+n = input("Enter the number of contacts : ")
+for i in range(0, n):
+    name1 = raw_input("Enter your name: ")
+    num = input("Enter your phone number: ")
     list1.extend([name1])
     list2.extend([num])
 
     # to convert two list into dictionary
-    dict1=dict(zip(Contact1,Contact2))
+    dict1 = dict(zip(list1, list2))
     print dict1
     print """
          1:Create a contact
          2:Read a contact
          3:Delete a contact
          4:Update a contact
-         
+
 choice=input("Enter your choice")
 def Create(dict1):
     name3=raw_input("Enter the new name you want to create: ")
     num3=input("Enter the number: ")
     dict1[name3]=num3
     print dict1
-def Read(dict1,n,Contact1,temp):
-    name2=raw_input("Enter the name whose number is to be found: ")
+def Read(dict1,n,list1,temp):
+    name2=raw_input("Enter the name whose number is to be read: ")
     for i in range(0,n):
-        if Contact1[i]==name2:
+        if list1[i]==name2:
             temp=i
     if temp!=100:
-        print "Number is : ",Contact2[temp]
+        print "Number is : ",list2[temp]
 def delete(dict1):
     name4=raw_input("Enter the name you want to delete: ")
     del dict1[name4]
     print dict1
-def update(dict1,n,Contact1):
+def update(dict1,n,list1):
     name5=raw_input("Enter the name which you want to update: ")
     for i in range(0,n):
         if list1[i]==name5:
